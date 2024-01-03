c97bc25791a42ff071f13e78647c15276b41b0fd
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-01-03 08:57:16 -0600

    Added aou.reader as dependency; updated .write_to_bucket doc

--- R/write_to_bucket.R
+++ R/write_to_bucket.R
@@ -1,4 +1,7 @@
 #' Internal function used to write dataset to workspace bucket and local file system
+#' @param dat the data to write
+#' @param output_folder the folder to write to
+#' @param name the name of the file to write (without extension)
 .write_to_bucket <- function(dat,output_folder,name)
 {
     if (missing(output_folder))
