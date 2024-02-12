0c10d1584b8a19aa5eef6afad63de2d1833be304
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-12 13:34:07 -0600

    education name was wrong

--- R/education.R
+++ R/education.R
@@ -6,6 +6,6 @@ education <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NUL
 {
     result <- aou.reader::survey_query("1585940",anchor_date_table,before,after)
     colnames(result) <- c("person_id","education_status","education_entry_date")
-    .write_to_bucket(result,output_folder,"education_query")
+    .write_to_bucket(result,output_folder,"education")
 }
 
