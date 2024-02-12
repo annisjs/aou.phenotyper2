f782b0845c118562fd1bbea44cb46ba5bbcc8576
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-12 13:06:20 -0600

    fixed arguments

--- R/alcohol.R
+++ R/alcohol.R
@@ -11,6 +11,6 @@ alcohol <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
     result <- aou.reader::survey_query("1586198",anchor_date_table,before,after)
     colnames(result) <- c("person_id","alcohol_status","alcohol_entry_date")
-    .write_to_bucket(result,anchor_date_table,before,after)
+    .write_to_bucket(result,output_folder,"alcohol")
 }
 
