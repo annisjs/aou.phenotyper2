b23938da3b4f7d37ccea506555ce391f449eeca0
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-15 10:59:14 -0600

    pregnancy and bariatric surgery algorithms

---
+++ R/bariatric_surgery.R
@@ -0,0 +1,23 @@
+#' Bariatric surgery
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return output_folder/bariatric_surgery.csv
+#' @details At least 1 ICD code
+#'
+#' ICD9: "V22.%","V23.%"
+#'
+#' ICD10: "Z33.%"
+#' @export
+bariatric_surgery <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+    cpt_codes <- c("43644","43645","43775","43845","43846","43847")
+    cpt_df <- aou.reader::cpt_query(cpt_codes,anchor_date_table,before,after)
+    cpt_df <- cpt_df[order(condition_start_date)]
+    cpt_df <- cpt_df[,.(bariatric_surgery_status = length(condition_start_date) > 0,
+                        bariatric_surgery_entry_date = condition_start_date[1]),
+                      .(person_id)]
+    .write_to_bucket(cpt_df,output_folder,"bariatric_surgery")
+}
