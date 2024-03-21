255e6a201cf23f6b8c8ee921a5a53e7cb0ce0af9
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-03-21 14:44:36 -0500

    adjusting column names

--- R/first_medical_encounter.R
+++ R/first_medical_encounter.R
@@ -10,6 +10,6 @@
 first_medical_encounter <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
     result <- aou.reader::medical_encounter_query("first",anchor_date_table,before,after)
-    colnames(result) <- c("person_id","first_medical_encounter")
+    colnames(result) <- c("person_id","first_medical_encounter_entry_date")
     .write_to_bucket(result,output_folder,"first_medical_encounter")
 }
