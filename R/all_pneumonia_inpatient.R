e17620559e108948c844697165c49a057751db0e
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-04-26 12:13:52 -0500

    fixing function name

--- R/all_pneumonia_inpatient.R
+++ R/all_pneumonia_inpatient.R
@@ -12,7 +12,7 @@
 #' ICD10:"J09.X1", "J09.X2", "J10.00", "J10.01", "J10.08", "J10.1", "J11.0", "J11.08", "J11.1", "J12.0", "J12.1", "J12.2", "J12.3", 
 #'        "J12.81", "J12.89", "J12.9", "J13", "J13.%", "J14", "J14.%", "J15.3", "J15.4", "J15.7", "J15.9", "J16.0", "J16.8", "J18.0", "J18.1", "J18.8", "J18.9"
 #' @export
-all_pneumonia <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+all_pneumonia_inpatient <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
   icd9_codes <- c("480","480.%","481","481.%","482","482.%","483","483.%","484","484.%","485","485.%","486","486.%","487.0","488.11","488.81")
   icd10_codes <- c("J09.X1", "J09.X2", "J10.00", "J10.01", "J10.08", "J10.1", "J11.0", "J11.08", "J11.1", "J12.0", "J12.1", "J12.2", "J12.3", 
