f1ac1d69e9c5fb7012ebe49784b747da6c61300c
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-04-26 11:57:25 -0500

    Fixing sepsis doc

--- R/all_sepsis.R
+++ R/all_sepsis.R
@@ -1,10 +1,10 @@
-#' Sepsis
+#' All Sepsis
 #'
 #' @param output_folder the folder to write the output
 #' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
-#' @return output_folder/sepsis.csv
+#' @return output_folder/all_sepsis.csv
 #' @details 
 #' Definition:
 #' 
