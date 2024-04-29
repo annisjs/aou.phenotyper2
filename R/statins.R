042b3807f3b3e318431163246ef9e6377627c3f2
Cihan <adnancihan.cakar@vumc.org>
2024-04-29 11:08:44 -0400

    adds status column to statins query

--- R/statins.R
+++ R/statins.R
@@ -1,7 +1,7 @@
 #' Statins
 #'
 #' @param output_folder the folder to write the output
-#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param anchor_date_table a data.frame containing three columns: person_id, anchor_date, statins_status. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
 #' @return output_folder/beta_blockers.csv
@@ -14,6 +14,7 @@ statins <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
   meds <- c("atorvastatin","lipitor","simvastatin","zocor","rosuvastatin","crestor","pravastatin",
             "pravachol","fluvastatin","lescol","lovastatin","mevacor","vytorin","caduet")
   result <- aou.reader::med_query(meds,anchor_date_table,before,after)
-  result <- result[,.(statins_entry_date = min(drug_exposure_start_date)),.(person_id)]
+  result <- result[,.(statins_status = length(drug_exposure_start_date) > 0,
+                      statins_entry_date = min(drug_exposure_start_date)),.(person_id)]
   .write_to_bucket(result,output_folder,"statins")
 }
