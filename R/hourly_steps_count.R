bf9e44bf6a63af651a4135c919a86d156ae5093b
Cihan <adnancihan.cakar@vumc.org>
2024-05-21 16:15:30 -0400

    Creates hourly_steps_count algorithm

---
+++ R/hourly_steps_count.R
@@ -0,0 +1,16 @@
+#' Hourly steps count at each day
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' 
+#' @details Step counts at each day and each hour
+#' @return output_folder/hourly_steps_count.csv
+#' @import data.table stringr aou.reader bigrquery
+#' @export
+hourly_steps_count <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+  result <- aou.reader::hourly_steps_query(anchor_date_table,before,after)
+  .write_to_bucket(result,output_folder,"hourly_steps_count")
+}
