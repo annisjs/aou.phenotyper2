be192f81baf7a2891dac456c1ebb5ee12921bdc5
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-05-22 09:45:33 -0500

    all inpatient icd codes

---
+++ R/all_inpatient_icd.R
@@ -0,0 +1,14 @@
+#' All Inpatient ICD codes
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return output_folder/all_inpatient_icd.csv
+#' @export
+all_inpatient_icd <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+    result_all <- aou.reader::inpatient_icd_query(NULL,output_folder,anchor_date_table,before,after)
+    colnames(result_all) <- c("person_id","all_inpatient_icd_entry_date","all_inpatient_icd_code")
+    .write_to_bucket(result_all,output_folder,"all_inpatient_icd")
+}
