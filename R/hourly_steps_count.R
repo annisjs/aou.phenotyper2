2aa6170fb83410335e108b20acfe4e836091453f
Cihan <adnancihan.cakar@vumc.org>
2024-06-19 10:34:35 -0400

    adds cohort subsetting to hourly_steps_count

--- R/hourly_steps_count.R
+++ R/hourly_steps_count.R
@@ -1,6 +1,7 @@
 #' Hourly steps count at each day
 #'
 #' @param output_folder the folder to write the output
+#' @param cohort a vector of ids to limit the query
 #' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
@@ -9,8 +10,8 @@
 #' @return output_folder/hourly_steps_count.csv
 #' @import data.table stringr aou.reader bigrquery
 #' @export
-hourly_steps_count <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+hourly_steps_count <- function(output_folder, cohort=NULL,anchor_date_table=NULL,before=NULL,after=NULL)
 {
-  result <- aou.reader::hourly_steps_query(anchor_date_table,before,after)
+  result <- aou.reader::hourly_steps_query(anchor_date_table=anchor_date_table,before=before,after=after,cohort = cohort)
   .write_to_bucket(result,output_folder,"hourly_steps_count")
 }
