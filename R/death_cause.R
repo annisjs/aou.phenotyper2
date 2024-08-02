7ff7431589878ebfef7838f614d6282b184cf4f1
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-08-02 13:51:27 -0500

    death cause algorithm

---
+++ R/death_cause.R
@@ -0,0 +1,13 @@
+#' Death cause
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return output_folder/death_cause.csv
+#' @export
+death_cause <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+    result_all <- aou.reader::death_cause_query(anchor_date_table,before,after)
+    .write_to_bucket(result_all,output_folder,"death_cause")
+}
