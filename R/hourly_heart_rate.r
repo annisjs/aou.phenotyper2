2149546c490c92d73c985a75f7caf3648035c35f
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-09-18 12:49:58 -0500

    hourly avg and standard dev of heart rate

---
+++ R/hourly_heart_rate.r
@@ -0,0 +1,17 @@
+#' Hourly heart rate average and standard deviation at each day
+#'
+#' @param output_folder the folder to write the output
+#' @param cohort a vector of ids to limit the query
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' 
+#' @details Average and standard deviation of heart rate at each day and each hour
+#' @return output_folder/hourly_heart_rate.csv
+#' @import data.table stringr aou.reader bigrquery
+#' @export
+hourly_heart_rate <- function(output_folder, cohort=NULL,anchor_date_table=NULL, before=NULL, after=NULL)
+{
+  result <- aou.reader::hourly_heart_rate(anchor_date_table=anchor_date_table, before=before, after=after, cohort=cohort)
+  .write_to_bucket(result,output_folder,"hourly_heart_rate")
+}
