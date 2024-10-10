4f52a7fe9e7f8f7b9866f196a0516dc36cbe27eb
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-10-10 09:00:38 -0500

    Semaglutide and liraglutide algorithms

---
+++ R/liraglutide.R
@@ -0,0 +1,19 @@
+#' Liraglutide
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return CSV saved to output_folder/liraglutide.csv
+#' @details Meds: "liraglutide","saxenda","victoza"
+#' @export
+liraglutide <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+	meds <- c("liraglutide","saxenda","victoza")
+	result <- aou.reader::med_query(meds,anchor_date_table,before,after)
+	result <- result[order(drug_exposure_start_date)]
+	result <- result[,.(liraglutide_entry_date = drug_exposure_start_date[1],
+						liraglutide_status = TRUE),
+					  .(person_id)]
+	.write_to_bucket(result,output_folder,"liraglutide")
+}
