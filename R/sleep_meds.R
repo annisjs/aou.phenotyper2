be1a6ec00de3b7c64a54bfd296817c7c0a2993e6
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-10-25 11:17:17 -0500

    row_num set to NULL not FALSE

--- R/sleep_meds.R
+++ R/sleep_meds.R
@@ -28,7 +28,7 @@ sleep_meds <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NU
     dt <- dt[order(drug_exposure_start_date)]
     dt <- dt[, row_num := 1:.N, .(person_id, med_class)]
     dt <- dt[rown_num == 1]
-    dt[, row_num := FALSE]
+    dt[, row_num := NULL]
     dt[, status := TRUE]
     dt_cast <- dcast(dt, person_id ~ med_class, value.var = c("drug_exposure_start_date","status"))
     med_date_cols <- paste0("drug_exposure_start_date_", med_classes)
