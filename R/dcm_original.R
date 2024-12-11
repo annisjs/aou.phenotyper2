c17e2d33a11ceba17fbcc0d51a4d9702d009e333
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-12-11 13:28:57 -0600

    ensuring same type

--- R/dcm_original.R
+++ R/dcm_original.R
@@ -136,8 +136,8 @@ dcm_original <- function(output_folder,anchor_date_table=NULL,before=NULL,after=
             condition_source_value = cpt_code
             )
   pci_snomeds <- aou.reader::snomed_query(pci_snomeds_list,anchor_date_table,before,after)
-  print(head(pci_cpts))
-  print(head(pci_snomeds))
+  pci_cpts$condition_start_date <- as.Date(pci_cpts$condition_start_date)
+  pci_snomeds$condition_start_date <- as.Date(pci_snomeds$condition_start_date)
   pci <- rbindlist(list(pci_cpts,pci_snomeds),use.names=TRUE,fill=TRUE)
   pci$pci_status <- TRUE
 
