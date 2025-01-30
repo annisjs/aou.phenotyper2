5cdedc4a89751d0ee606386b40e79a99fe241e68
Brandon Lowery <brandon.d.lowery@vumc.org>
2025-01-30 11:27:50 -0600

    date type

--- R/hermes_heart_failure.R
+++ R/hermes_heart_failure.R
@@ -131,6 +131,8 @@ hermes_heart_failure <- function(output_folder,anchor_date_table=NULL,before=NUL
   #setnames(pci_cpts, c("entry_date", "cpt_code"), c("condition_start_date","condition_source_value"))
   colnames(pci_cpts) <- c("person_id","condition_source_value","condition_start_date") 
   pci_cpts <- pci_cpts[, c("person_id","condition_start_date","condition_source_value")]
+  pci_cpts$condition_start_date <- as.Date(pci_cpts$condition_start_date)
+  pci_snomeds$condition_start_date <- as.Date(pci_snomeds$condition_start_date)
 
   pci <- rbindlist(list(pci_cpts,pci_snomeds),use.names=TRUE,fill=TRUE)
   pci$pci_status <- TRUE
