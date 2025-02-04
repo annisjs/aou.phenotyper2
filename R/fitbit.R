ff6216725da3f2852e955fa9f9521703eaafd02c
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-02-04 10:56:14 -0600

    keep shards option

--- R/fitbit.R
+++ R/fitbit.R
@@ -12,5 +12,5 @@
 fitbit <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
     result <- aou.reader::fitbit_query(anchor_date_table,before,after)
-    .write_to_bucket(result,output_folder,"fitbit")
+    .write_to_bucket(result,output_folder,"fitbit",TRUE,"fitbit_query.csv")
 }
--- R/write_to_bucket.R
+++ R/write_to_bucket.R
@@ -2,16 +2,28 @@
 #' @param dat the data to write
 #' @param output_folder the folder to write to
 #' @param name the name of the file to write (without extension)
+#' @param keep_shards if TRUE, original shards from bigquery will be kept and moved to folder under designated name
+#' @param query_folder_origin if keep_shards is TRUE, must specify the name of the folder where the query is written to
 #' @import stringr
-.write_to_bucket <- function(dat,output_folder,name)
+.write_to_bucket <- function(dat,output_folder,name,keep_shards=FALSE,query_folder_origin=NULL)
 {
     dat <- dat[!duplicated(dat)]
     if (missing(output_folder))
     {
         return(dat)
     } else {
-        file <- paste0(name,".csv")
-        data.table::fwrite(dat,file)
-        aou.bucket::cp_to_bucket(file, stringr::str_glue("{output_folder}/{file}"))
+        if (keep_shards)
+        {
+            my_bucket <- Sys.getenv("WORKSPACE_BUCKET")
+            if(stringr::str_glue("{name}.csv") %in% basename(system(stringr::str_glue("gsutil ls {my_bucket}/{output_folder}"), intern = T)))
+            {
+                system(stringr::str_glue("gsutil rm -rf {output_folder}/{name}.csv"), intern = T)
+            }
+            system(stringr::str_glue("gsutil mv {my_bucket}/{query_folder_origin}/* {output_folder}/{name}.csv"),intern=T)
+        } else {
+            file <- paste0(name,".csv")
+            data.table::fwrite(dat,file)
+            aou.bucket::cp_to_bucket(file, stringr::str_glue("{output_folder}/{file}"))
+        }
     }
 }
