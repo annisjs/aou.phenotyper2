517cdba8de20e7d3ce7965e2f31974197581573e
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-02-04 12:32:21 -0600

    smoking current algorithm rm extra arg

--- R/smoking_current.R
+++ R/smoking_current.R
@@ -9,7 +9,7 @@
 #' @export
 smoking_current <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
-  result <- aou.reader::survey_query("1585860",anchor_date_table,output_folder,before,after)
+  result <- aou.reader::survey_query("1585860",anchor_date_table,before,after)
   colnames(result) <- c("person_id","smoking_current_status","smoking_current_entry_date")
   .write_to_bucket(result,output_folder,"smoking_current")
 }
