16250191439e045ac986be5ce10db93343c60b22
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-05-16 09:09:12 -0500

    ibs, ibd algorithms

--- R/ibd.R
+++ R/ibd.R
@@ -1,26 +1,26 @@
-#' Inflammatory bowel syndrome (IBS)
+#' Inflammatory bowel disease (IBD)
 #'
 #' @param output_folder the folder to write the output
 #' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
-#' @return output_folder/ibs.csv
+#' @return output_folder/ibd.csv
 #' @details At least 1 ICD code:
 #'
-#' ICD9: 564.1
+#' ICD9: 555.0, 555.1, 555.2, 555.9, 556.0, 556.1, 556.2, 556.9, 
 #'
-#' ICD10: K59.0, K58.1, K58.2, K58.9
+#' ICD10: K50.0, K50.1, K50.8, K50.9, K51.0, K51.1, K51.2, K51.9
 #' @export
-ibs <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+ibd <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
-    icd9_codes <- c("564.1")
-    icd10_codes <- c("K59.0", "K58.1", "K58.2", "K58.9")
+    icd9_codes <- c("555.0", "555.1", "555.2", "555.9", "556.0", "556.1", "556.2", "556.9")
+    icd10_codes <- c("K50.0", "K50.1", "K50.8", "K50.9", "K51.0", "K51.1", "K51.2", "K51.9")
     result_icd9 <- aou.reader::icd9_query(icd9_codes,anchor_date_table,before,after)
     result_icd10 <- aou.reader::icd10_query(icd10_codes,anchor_date_table,before,after)
     result_all <- rbind(result_icd9,result_icd10)
     result_all <- result_all[order(condition_start_date)]
-    result_all <- result_all[,.(ibs_entry_date = condition_start_date[1],
-                                ibs_status = TRUE),
+    result_all <- result_all[,.(ibd_entry_date = condition_start_date[1],
+                                ibd_status = TRUE),
                             .(person_id)]
-    .write_to_bucket(result_all,output_folder,"ibs")
+    .write_to_bucket(result_all,output_folder,"ibd")
 }
