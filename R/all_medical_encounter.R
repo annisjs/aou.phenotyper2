10222bad02743cb6c6ff1813ff1b35ccd716e5fe
cakarac <adnancihan.cakar@vumc.org>
2025-07-08 13:09:27 -0500

    minor document update

--- R/all_medical_encounter.R
+++ R/all_medical_encounter.R
@@ -4,7 +4,7 @@
 #' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
-#' @return output_folder/all_medical_encounter.csv
+#' @return output_folder/all_medical_encounter_*.csv
 #' @import stringr bigrquery data.table
 #' @export
 all_medical_encounter <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
