30fc64d722d9e2bc5610591ea41d22e106d81640
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-07-18 15:28:28 -0500

    adding count to all icd9/10 codes algorithms

--- R/all_icd9_codes.R
+++ R/all_icd9_codes.R
@@ -9,6 +9,6 @@
 all_icd9_codes <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
 {
     result_all <- aou.reader::icd9_query(NULL,anchor_date_table,before,after)
-    colnames(result_all) <- c("person_id","all_icd9_codes_entry_date","all_icd9_codes_value")
+    colnames(result_all) <- c("person_id","all_icd9_codes_entry_date","all_icd9_codes_value","all_icd9_codes_count")
     .write_to_bucket(result_all,output_folder,"all_icd9_codes")
 }
