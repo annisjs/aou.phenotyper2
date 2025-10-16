582649d3975ad577998d7035193bc8d1a5970198
cakarac <adnancihan.cakar@vumc.org>
2025-10-16 12:17:16 -0500

    all hospitalizations functions fix

--- R/all_cad_hospitalizations.R
+++ R/all_cad_hospitalizations.R
@@ -11,7 +11,7 @@ all_cad_hospitalizations <- function(output_folder,anchor_date_table=NULL,before
     icd_codes <- c("410","410.%","411","411.%","412","412.%","413","413.%","414","414.%","V45.82","I25.1%")
     result <- aou.reader::hospitalization_query(icd_codes,anchor_date_table,before,after)
     result <- result[,c("person_id","hospitalization_entry_date")]
-    result[,all_hf_hospitalization_entry_date := hospitalization_entry_date]
+    result[,all_cad_hospitalization_entry_date := hospitalization_entry_date]
     result <- result[,c("person_id","all_cad_hospitalizations_entry_date")]
     result <- result[!duplicated(result)]
     .write_to_bucket(result,output_folder,"all_cad_hospitalizations")
