3bfee016e0a1a376975f017ffe543226db633610
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2026-04-01 13:45:57 -0500

    Adds all GLP1 medications algorithm

---
+++ R/all_glp1_meds.R
@@ -0,0 +1,21 @@
+#' All GLP1 Meds
+#'
+#' @param output_folder the folder to write the output
+#' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
+#' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
+#' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
+#' @return CSV saved to output_folder/all_glp1_meds.csv
+#' @details Meds: "lixisenatide","adlyxin","albiglutide","tanzeum","exenatide","bydureon bcise","bydureon","byetta","liraglutide","saxenda","victoza",
+#'    "dulaglutide","trulicity","semaglutide","ozempic","rybelsus","tirzepatide","mounjaro","zepbound"
+#' @export
+all_glp1_meds <- function(output_folder,anchor_date_table=NULL,before=NULL,after=NULL)
+{
+    meds <- c("lixisenatide","adlyxin","albiglutide","tanzeum","exenatide","bydureon bcise","bydureon","byetta","liraglutide","saxenda","victoza",
+    "dulaglutide","trulicity","semaglutide","ozempic","rybelsus","tirzepatide","mounjaro","zepbound")
+    result <- aou.reader::med_query(meds,anchor_date_table,before,after)
+    result <- result[order(drug_exposure_start_date)]
+    data.table::setnames(result, "drug_exposure_start_date", "all_glp1_meds_entry_date")
+    data.table::setnames(result, "drug_name", "all_glp1_meds_name")
+    result <- result[, c("person_id", "all_glp1_meds_entry_date", "all_glp1_meds_name")]
+    .write_to_bucket(result,output_folder,"all_glp1_meds")
+}
